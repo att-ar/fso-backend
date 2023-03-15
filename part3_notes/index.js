725284928c982059b4a61b7c76983a047bde126e
Attar Aziz <attar_a@outlook.com>
2023-03-15 17:01:59 -0400

    Part 3 notes until ex 3.19 (Mongoose Validation)

--- part3_notes/index.js
+++ part3_notes/index.js
@@ -69,7 +69,7 @@ const generateId = () => {
     return maxId + 1;
 };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     const body = request.body;
 
     if (body.content === undefined) {
@@ -81,9 +81,11 @@ app.post("/api/notes", (request, response) => {
         important: body.important || false,
     });
 
-    note.save().then((savedNote) => {
-        response.json(savedNote);
-    });
+    note.save()
+        .then((savedNote) => {
+            response.json(savedNote);
+        })
+        .catch((error) => next(error));
 });
 
 //my implementation:
@@ -106,14 +108,13 @@ app.post("/api/notes", (request, response) => {
 // for the phonebook but i had upsert: true
 // so that it doesnt add only modifies existing things
 app.put("/api/notes/:id", (request, response, next) => {
-    const body = request.body;
+    const { content, important } = request.body;
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    };
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: "query" }
+    )
         .then((updatedNote) => {
             response.json(updatedNote);
         })
@@ -152,6 +153,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).send(error.message);
     }
 
     next(error);
